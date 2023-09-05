0093be9005164ed815e3698f678831bcaa3f9a7e
keshav15 <keshavbajaj4444@gmail.com>
2023-09-05 23:20:56 +0530

    Commit

---
+++ app/llmapi.py
@@ -0,0 +1,28 @@
+from poe_api_wrapper import Poe
+from poe_api_wrapper import PoeApi
+import json
+token = "-AWr3euMgNBkIZzqFvykXw%3D%3D"
+
+
+
+
+# client = PoeApi(token)
+# with open("/home/keshav/Desktop/portfolio_app/app/configtemplate.json","r") as configfile:
+#     config_data=json.load(configfile)
+
+
+
+# bot = "chinchilla"
+# message = f"This is my resume data {resumedata} and I want this data to be in this json format {config_data}"
+
+#     # Create new chat thread
+#     # Streamed example:
+# for chunk in client.send_message(bot, message):
+#     pass
+# print(chunk["text"])
+
+
+
+# Poe.chat_with_bot(token)
+
+
--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,24 @@ from werkzeug.utils import secure_filename
 import PyPDF2
 import json
 import os
-
 from app import app
 from app.utils import allowed_file, extract_resume_data
 
+from poe_api_wrapper import Poe
+from poe_api_wrapper import PoeApi
+import json
+token = "-AWr3euMgNBkIZzqFvykXw%3D%3D"
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+client = PoeApi(token)
+with open("/home/keshav/Desktop/portfolio_app/app/configtemplate.json","r") as configfile:
+    config_data=json.load(configfile)
+
+
+
+
+
 
 @app.route('/')
 def index():
@@ -32,6 +44,15 @@ def upload():
         
         extracted_data = extract_resume_data(temp_path)
         print(extracted_data)
+        
+        bot = "chinchilla"
+        message = f"This is my resume data {extracted_data} and I want this data to be in this json format{config_data}  so just convert that data into this format.. and please give me just json as output.. and dont add anything extratext as I would directly store this in some jsonfile"
+
+        # Create new chat thread
+        # Streamed example:
+        for chunk in client.send_message(bot, message):
+            pass
+        print(chunk["text"])
         os.remove(temp_path)
         
         return jsonify(extracted_data), 200
